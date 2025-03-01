edee62940a98b1ff100049fa4bc13db7d30f7525
gossamer <gossameralbumcover@gmail.com>
2025-03-01 23:27:08 +0300

    added aceternity text generate effect

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -12,6 +12,8 @@ import {
 } from "@ant-design/icons";
 import Image from "next/image";
 import { createStyles } from "antd-style";
+import { motion, stagger, useAnimate } from "framer-motion";
+import { cn } from "@/utils";
 
 // Import the custom BackgroundLines component
 import { BackgroundLines } from "../../BackgroundLines.js";
@@ -357,7 +359,11 @@ const Home = () => {
 										...messageBubbleStyle,
 										alignSelf: msg.role === "user" ? "flex-end" : "flex-start",
 									}}>
-									{msg.text}
+									{msg.role === "ai" ? (
+										<TextGenerateEffect words={msg.text} />
+									) : (
+										msg.text
+									)}
 								</div>
 							))}
 							<div ref={messagesEndRef} />
@@ -422,6 +428,58 @@ const Home = () => {
 				</div>
 			</div>
 		</BackgroundLines>
+	);
+};
+
+export const TextGenerateEffect = ({
+	words,
+	className,
+	filter = true,
+	duration = 0.5,
+}) => {
+	const [scope, animate] = useAnimate();
+	let wordsArray = words.split(" ");
+	useEffect(() => {
+		animate(
+			"span",
+			{
+				opacity: 1,
+				filter: filter ? "blur(0px)" : "none",
+			},
+			{
+				duration: duration ? duration : 1,
+				delay: stagger(0.03),
+			}
+		);
+	}, [scope.current]);
+
+	const renderWords = () => {
+		return (
+			<motion.div ref={scope}>
+				{wordsArray.map((word, idx) => {
+					return (
+						<motion.span
+							key={word + idx}
+							className="dark:text-white text-black opacity-0"
+							style={{
+								filter: filter ? "blur(10px)" : "none",
+							}}>
+							{word}{" "}
+						</motion.span>
+					);
+				})}
+			</motion.div>
+		);
+	};
+
+	return (
+		<div className={cn("font-bold", className)}>
+			<div className="mt-4">
+				<div className=" dark:text-white text-black text-2xl leading-snug tracking-wide">
+					{renderWords()}
+				</div>
+			</div>
+		</div>
 	);
 };
 
